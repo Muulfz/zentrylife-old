90aed3da4e3bd6095065507d2e0d67e473645abb
Muulfz <mattreisro@gmail.com>
2018-12-01 13:49:07 -0200

    Fivem servers files!

---
+++ resources/[Premium]/ZentryCsharp_Utils/__resource.lua
@@ -0,0 +1,8 @@
+client_script {
+    "ZentryCSharp_Utils.Client.net.dll",
+    "ZentryCSharp_Utils.Shared.net.dll"
+}
+server_script {
+    "ZentryCSharp_Utils.Server.net.dll",
+    "ZentryCSharp_Utils.Shared.net.dll"
+}
