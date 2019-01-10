f199c81c23f2d634c4bceb502af3882708620f3e
MarmotaGit <jaimeadanielfilho@hotmail.com>
2019-01-10 03:06:23 -0200

    A lot of updates

---
+++ resources/[zRP]/zrp_vehsealer/__resource.lua
@@ -0,0 +1,21 @@
+ui_page "HTML/index.html"
+
+client_scripts {
+    "@zrp/lib/utils.lua",
+    "client.lua"
+}
+server_scripts {
+    "@zrp/lib/utils.lua",
+    "server.lua"
+}
+
+files {
+    "HTML/index.html",
+    "HTML/cars.html",
+    "HTML/CSS/font-awesome.min.css",
+    "HTML/CSS/sweetalert2.css",
+    "HTML/CSS/style.css",
+    "HTML/JS/sweetalert2.js",
+    "HTML/JS/index.js",
+    "HTML/JS/cars.js"
+}
